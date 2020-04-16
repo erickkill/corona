77b9dcb51bf86b6dc03490b57a160a3ce4095bea
Erick Kill <31928043+erickkill@users.noreply.github.com>
2020-04-16 12:09:20 -0300

    Update script_jorge.py

--- script_jorge.py
+++ script_jorge.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 #Abrindo o dataset
-corona = pd.read_excel("diretório local com o xlsx")
+corona = pd.read_excel("diretório local com o .xlsx")
 
 #Fixando a coluna "Municipio" como index e convertendo as linhas em coluna, utilizando a função pivot
 df = corona.pivot(index='Municipio', columns='Status', values='Total')
@@ -20,10 +20,10 @@ df.reset_index(level=0, inplace=True)
 df.rename_axis(None, inplace=True)
 
 #Exportando para excel
-df.to_csv(f'diretório local apontando o c.sv',index=False, encoding='utf-8-sig')
+df.to_csv(f'diretório local apontando o .csv',index=False, encoding='utf-8-sig')
 
 #Adicionando como layer no QGIS
-layer_corona = QgsVectorLayer('diretório local com o csv gerado', 'Corona', 'ogr')
+layer_corona = QgsVectorLayer('diretório local com o .csv gerado', 'Corona', 'ogr')
 QgsProject.instance().addMapLayer(layer_corona)
 
 #Setando o encoding UTF-8 para a tabela no QGIS
